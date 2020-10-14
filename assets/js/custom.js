6b07c43c2ec014024b17dd709e655fae530bfaa7
TIANA <tt@tianas-mbp.lan>
2020-10-13 22:01:30 -0700

    my first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,14 @@
+console.log("Hello from custom.js!");
+
+function myFunction() {
+    alert("Welcome!");
+  }
+
+  function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
