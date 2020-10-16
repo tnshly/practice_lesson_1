8e908ed343505409e1f1e924794ffb959cf134d4
TIANA <tt@tianas-mbp.lan>
2020-10-16 11:20:12 -0700

    WIPx2

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,21 @@
 console.log("Hello from custom.js!");
 
 function myIcon() {
-    alert("Welcome!");
-  }
+  alert("Welcome!");
+}
 
-  function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
+}
 
-  function sConsole(event) {
-    event.preventDefault();
-    var data = document.getElementById("data");
-    console.log(data.value);
-    
-  }
+function sConsole(event) {
+  event.preventDefault();
+  var data = document.getElementById("data");
+  console.log(data.value);
+
+}
